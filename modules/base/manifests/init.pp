4ecd62bd8bf6b7dd7c6ca28d3e6a4f2e38c29ba0
Kuenstler <KevinKuenstler@hotmail.de>
2016-08-11 14:16:32 +0200

    updated init

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,32 +1,51 @@
 class base (
+	$vm_shared_folder = undef,
 	$timezone         = undef,
+	$ntp_servers			= undef,	
 ) {
-
+	validate_string($timezone)
+	validate_array($ntp_servers)
 	include base::services
 	include base::execs
 	include base::sshkeysroot
-  include base::packages
-  validate_string($timezone)
+	include base::packages
+	include base::files
 
 	realize Exec['yum_clean_all']
+	realize File['symlink_rootpuppet_to_etc_puppet']
+	
+	if ($vm_shared_folder != 'undef') {
+		file { 'symlink_/root/vm_to_shared-folder':
+			ensure => 'link',
+			path   => '/root/vm',
+			target => "${vm_shared_folder}",
+		}
+	}
+
+	# Service
 	realize Service['iptables']
 	realize Service['crond']
+	
+	# Alias
+	realize File_line['alias_vim']
+	realize File_line['alias_confluence_cdhome']
+	realize File_line['alias_confluence_cdinstall']
+	realize File_line['alias_confluence_log']
+	realize File_line['alias_jira_checklock']
+	realize File_line['alias_jira_cdhome']
+	realize File_line['alias_jira_cdinstall']
+	realize File_line['alias_jira_log']
+	realize File_line['alias_puppet_run']
+	realize File_line['alias_puppet_lookup']
 
-# resolv.conf
-
-	file { "link_localtime_to_$timezone":
-		path    => '/etc/localtime',
-		ensure  => 'link',
-		target  => "/usr/share/zoneinfo/$timezone",
-		force   => true,
-	}
-
+	# Timezone
+	realize File["link_localtime_to_$timezone"]
 	class { '::ntp':
-		servers => [ '0.centos.pool.ntp.org', '1.centos.pool.ntp.org','2.centos.pool.ntp.org'],
+		servers => $ntp_servers,
 	}
 
 
-	# Update libs if required
+	# Update libs if an rpm has been installed
 	file {  "/var/lib/rpm/Packages":
 		owner   => root,
 		group   => root,
